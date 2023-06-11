69a45fa9a57704ca134af6f2f4a6819163b16c57
dtkhoi <dtkhoi@cmcglobal.vn>
2023-06-12 04:44:40 +0700

    index js

---
+++ index.js
@@ -0,0 +1,33 @@
+var a = 1;
+var b = "string";
+
+var c = a + b;
+console.log(c);
+
+function add(a,b){
+    return a + b;
+}
+
+/*
+console.log(number)
+var number = 10;
+
+console.log(number);
+*/
+
+console.log(x);
+var x = 1;
+
+var arr = [1,2,3,4,5];
+var arrCopy = [...arr];
+
+for (let i = 0; i < arrCopy.length; i++) {
+    let element = arrCopy[i];
+    console.log(element);
+}
+
+console.log(element);
+
+
+
+
